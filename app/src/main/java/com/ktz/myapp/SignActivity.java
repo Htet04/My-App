c337b783e58e11c38c69d538a71f326c20d5facd
クンテインゾー <87855489+Htet04@users.noreply.github.com>
2022-11-15 12:07:00 +0630

    add SignActivity and make it launcher activity No functions code

--- app/src/main/java/com/ktz/myapp/SignActivity.java
+++ app/src/main/java/com/ktz/myapp/SignActivity.java
@@ -4,16 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.ktz.myapp.databinding.ActivityMainBinding;
+import com.ktz.myapp.databinding.ActivitySignBinding;
 
-public class MainActivity extends AppCompatActivity {
-
-    ActivityMainBinding binding;
+public class SignActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        ActivitySignBinding binding = ActivitySignBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
     }
 }
