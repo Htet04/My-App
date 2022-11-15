02bcc1adcb9e81e4b66291751c932119efbefe71
クンテインゾー <87855489+Htet04@users.noreply.github.com>
2022-11-15 12:48:13 +0630

    change view id to simple and initialize it

--- app/src/main/java/com/ktz/myapp/fragment/sign/SignUpFragment.java
+++ app/src/main/java/com/ktz/myapp/fragment/sign/SignUpFragment.java
@@ -8,12 +8,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.ktz.myapp.R;
 import com.ktz.myapp.databinding.FragmentSignUpBinding;
 
 public class SignUpFragment extends Fragment {
 
     FragmentSignUpBinding binding;
+    TextInputLayout nameLayout,emailLayout,passLayout;
+    TextInputEditText name,email,pass;
+    MaterialButton btnSignUp,btnSignIn;
 
     public SignUpFragment() {
         // Required empty public constructor
@@ -23,7 +29,21 @@ public class SignUpFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentSignUpBinding.inflate(getLayoutInflater());
+        // initialization of View Objects
+        initializer();
+
+
 
         return binding.getRoot();
+    }
+    private void initializer(){
+        nameLayout = binding.nameLayout;
+        emailLayout = binding.emailLayout;
+        passLayout = binding.passLayout;
+        name = binding.name;
+        email = binding.email;
+        pass = binding.pass;
+        btnSignUp = binding.btnSignUp;
+        btnSignIn = binding.btnSignIn;
     }
 }
